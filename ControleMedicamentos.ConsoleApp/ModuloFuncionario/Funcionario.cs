47a2876421826d7846d46178455ae3cb4c105d7f
Enzo <enzophi07@gmail.com>
2024-05-02 15:33:15 -0300

    update Módulo Funcionário

---
+++ ControleMedicamentos.ConsoleApp/ModuloFuncionario/Funcionario.cs
@@ -0,0 +1,51 @@
+﻿using ControleMedicamentos.ConsoleApp.Compartilhado;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ControleMedicamentos.ConsoleApp.ModuloFuncionario
+{
+    internal class Funcionario : EntidadeBase
+    {
+        public string nome { get; set; }
+        public string login { get; set; }
+        public string senha { get; set; }
+        public string cpf { get; set; }
+
+        public Funcionario(string nome, string login, string senha, string cpf)
+        {
+            this.nome = nome;
+            this.login = login;
+            this.senha = senha;
+            this.cpf = cpf;
+        }
+
+        public override string[] Validar()
+        {
+            string[] erros = new string[3];
+            int contadorErros = 0;
+
+            if (string.IsNullOrEmpty(nome.Trim()))
+                erros[contadorErros++] = ("O campo \"nome\" é obrigatório");
+
+            if (string.IsNullOrEmpty(cpf.Trim()))
+                erros[contadorErros++] = ("O campo \"cpf\" é obrigatório");
+
+            if (string.IsNullOrEmpty(login.Trim()))
+                erros[contadorErros++] = ("O campo \"login\" é obrigatório");
+
+            if (string.IsNullOrEmpty(senha.Trim()))
+                erros[contadorErros++] = ("O campo \"senha\" é obrigatório");           
+
+            string[] errosFiltrados = new string[contadorErros];
+
+            Array.Copy(erros, errosFiltrados, contadorErros);
+
+            return errosFiltrados;
+        }
+
+       
+    }
+}
---
+++ ControleMedicamentos.ConsoleApp/ModuloFuncionario/TelaFuncionario.cs
@@ -0,0 +1,67 @@
+﻿using ControleMedicamentos.ConsoleApp.Compartilhado;
+using ControleMedicamentos.ConsoleApp.ModuloMedicamento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ControleMedicamentos.ConsoleApp.ModuloFuncionario
+{
+    internal class TelaFuncionario : TelaBase
+    {
+        public TelaFuncionario(RepositorioBase repositorio, string nome)
+        {
+            this.repositorio = repositorio;
+            tipoEntidade = nome;
+        }
+
+        public override void VisualizarRegistros(bool exibirTitulo)
+        {
+            if (exibirTitulo)
+            {
+                ApresentarCabecalho();
+                Console.WriteLine("Visualizando Funcionarios...");
+            }
+
+            Console.WriteLine(
+                "\n{0, -10} | {1, -20} | {2, -20} | {3, -20} | {4, -20}",
+                "Id", "Nome", "CPF", "Login", "Senha"
+            );
+
+            EntidadeBase[] funcionariosCadastrados = repositorio.SelecionarTodos();
+
+            foreach (Funcionario funcionario in funcionariosCadastrados)
+            {
+                if (funcionario == null)
+                    continue;
+
+                Console.WriteLine(
+                    "{0, -10} | {1, -20} | {2, -20} | {3, -20} | {4, -20}",
+                    funcionario.Id, funcionario.nome, funcionario.cpf, funcionario.login, funcionario.senha
+                );
+            }
+            Console.ReadKey(true);
+        }
+
+        protected override EntidadeBase ObterRegistro()
+        {
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o CPF: ");
+            string cpf = Console.ReadLine();
+
+            Console.Write("Digite o login: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Digite a senha: ");
+            string senha = Console.ReadLine();
+
+
+
+            Funcionario funcionario = new Funcionario(nome, cpf, login, senha);
+
+            return funcionario;
+        }
+    }
+}
